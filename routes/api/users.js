9cae1065d329ef1410eab895fe80c9e48f8f3a35
Aqshola <aqsholafid2@gmail.com>
2020-09-29 14:40:02 +0700

    add jwt configuration for register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @troute  Get api/users
 // @desc    Register user
@@ -54,7 +56,25 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("user registered");
+      const payload = {
+        user: {
+          id: user._id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       return res.status(500).send("server error");
