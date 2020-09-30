d0cbf4bf7277f8d431c392e4a8ec1bf8565d6853
Aqshola <aqsholafid2@gmail.com>
2020-09-30 18:23:12 +0700

    add api to get my profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-// @troute  Get api/profile
-// @desc    Test route
-// @access  public
-router.get("/", (req, res) => {
+// @route  Get api/profile/me
+// @desc    Get current user profile
+// @access  Private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server Error");
+  }
   res.send("profile routes");
 });
 
