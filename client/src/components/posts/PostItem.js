dd819f3e4835d855ef92df75a5e28bf54d3d5c82
Aqshola <aqsholafid2@gmail.com>
2020-10-28 21:51:42 +0700

    add Post Component and link with Posts Component

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,72 @@
+import {
+  Avatar,
+  Box,
+  IconButton,
+  makeStyles,
+  Paper,
+  Typography,
+  Link as Links,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Button,
+  CardActionArea,
+} from "@material-ui/core";
+import React from "react";
+import Moment from "react-moment";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ChatBubbleOutlineOutlinedIcon from "@material-ui/icons/ChatBubbleOutlineOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+
+const style = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  },
+  card: {
+    width: "100%",
+  },
+  cardActions: {
+    display: "flex",
+  },
+  actionBtn: {
+    margin: theme.spacing(1),
+    width: "30%",
+  },
+}));
+
+export const PostItem = ({ post: { avatar, name, text, date } }) => {
+  const classes = style();
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={<Avatar src={avatar} />}
+        title={name}
+        subheader={<Moment fromNow>{date}</Moment>}
+      />
+      <CardContent>
+        <Typography variant="body2">{text}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button
+          className={classes.actionBtn}
+          startIcon={<ThumbUpAltOutlinedIcon />}
+        >
+          Like
+        </Button>
+        <Button
+          className={classes.actionBtn}
+          startIcon={<ChatBubbleOutlineOutlinedIcon />}
+        >
+          Comment
+        </Button>
+        <Button className={classes.actionBtn} startIcon={<ShareOutlinedIcon />}>
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,13 +3,47 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getPosts } from "../../action/post";
 import Spinner from "../layout/Spinner";
+import { Box, Container, Typography, makeStyles } from "@material-ui/core";
+import { PostItem } from "./PostItem";
+
+const style = makeStyles((theme) => ({
+  boxContent: {
+    width: "50%",
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
+  },
+}));
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
+  const classes = style();
+
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <div>Post</div>;
+  return (
+    <Container>
+      {loading ? (
+        <Spinner />
+      ) : posts.length > 0 ? (
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          flexWrap="wrap"
+          margin="auto"
+          marginY="20px"
+          className={classes.boxContent}
+        >
+          {posts.map((post) => (
+            <PostItem post={post} key={post._id} />
+          ))}
+        </Box>
+      ) : (
+        <Typography>No Posts</Typography>
+      )}
+    </Container>
+  );
 };
 
 Posts.propTypes = {
