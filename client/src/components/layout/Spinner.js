757d8b1fd09998497a286f4a9c44525cbefb2106
Aqshola <aqsholafid2@gmail.com>
2020-11-02 17:55:36 +0700

    fixing loading

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,8 +1,27 @@
 import React from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { Box, Container, Typography, makeStyles } from "@material-ui/core";
+
+const height = window.innerHeight;
+const style = makeStyles((theme) => ({
+  container: {
+    height: height,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    [theme.breakpoints.down("xs")]: {
+      height: height - 200,
+    },
+  },
+}));
 
 const Spinner = ({ ...rest }) => {
-  return <CircularProgress {...rest} disableShrink />;
+  const classes = style();
+  return (
+    <Container className={classes.container}>
+      <CircularProgress {...rest} disableShrink />;
+    </Container>
+  );
 };
 
 export default Spinner;
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -11,7 +11,7 @@ import {
 const initialState = {
   posts: [],
   post: null,
-  loaing: true,
+  loading: true,
   error: {},
 };
 
