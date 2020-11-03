2eb358fd87702a262546f7d24d347f4eab3f7700
Aqshola <aqsholafid2@gmail.com>
2020-11-03 15:03:46 +0700

    preparing for deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
-
+const path = require("path");
 // Connect database
 connectDB();
 
@@ -15,6 +15,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/post", require("./routes/api/post"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`running server`);
